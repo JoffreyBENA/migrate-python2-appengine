999d4f132e7d94b8d62a7ac0bfae6c25c90a6f99
Joffrey <joffreybena@gmail.com>
2023-03-28 20:47:14 +0200

    pyhton 3

--- mod0-baseline/appengine_config.py
+++ mod0-baseline/appengine_config.py
@@ -1,6 +1,8 @@
 from google.appengine.ext import vendor
+import os
 
 # Set PATH to your libraries folder.
-PATH = 'lib'
+PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+
 # Add libraries installed in the PATH folder.
 vendor.add(PATH)
